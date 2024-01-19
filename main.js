58e6e84fc6152b19de9729a07e57e6b8de88e12e
Purindu Weerawardana <Purindup@compello.com>
2024-01-19 17:09:50 +0530

    Initial setup

---
+++ main.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+const isMac = process.platform === "darwin";
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "GitOps",
+    width: 800,
+    height: 600,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
